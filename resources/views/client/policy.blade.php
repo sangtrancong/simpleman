e894b48c4081f964d8a0a0e057c2bfd61eeb7914
sangtrancong171196@gmail.com <sang1996>
2023-03-13 20:24:02 +0700

    updateL

--- resources/views/client/policy.blade.php
+++ resources/views/client/policy.blade.php
@@ -7,90 +7,38 @@
     <div class="child-content">
         <div class="container" style="padding:0 30px;text-align: justify">
             <div class="row" style="width: 1000px;">
-                <p> Website simplemanplus.com thuộc sở hữu của simplemanplus, là cơ quan kiểm soát dữ liệu của bạn
-                    dữ liệu cá nhân. </p>
-                <p> Chúng tôi đã thông qua Chính sách Bảo mật này, xác định cách chúng tôi xử lý thông tin được thu thập bởi
-                    simplemanplus.com, và cung cấp lý do tại sao chúng tôi phải thu thập dữ liệu cá nhân nhất định về
-                    bạn. Vì vậy,
-                    bạn phải đọc Chính sách Bảo mật này trước khi sử dụng trang web simplemanplus.com. </p>
-                <p> Chúng tôi chăm sóc dữ liệu cá nhân của bạn và cam kết đảm bảo tính bảo mật và an toàn của dữ liệu đó.
+                <p>The website simplemanplus.com is owned by simplemanplus, which is the data controller of your personal data. </p>
+                <p> We have adopted this Privacy Policy, which defines how we treat information collected by implemanplus.com, and provides why we must collect certain personal data about you. Therefore, you must read this Privacy Policy before using the simplemanplus.com website.</p>
+                <p> We take care of your personal data and are committed to ensuring its security and safety.
                 </p>
-                <p> Thông tin cá nhân mà chúng tôi thu thập: </p>
-                <p> Khi bạn truy cập vào simplemanplus.com, chúng tôi sẽ tự động thu thập một số thông tin nhất định về
-                    thiết bị của bạn,
-                    bao gồm thông tin về trình duyệt web của bạn, địa chỉ IP, múi giờ và một số cookie nhất định được cài
-                    đặt trên
-                    thiết bị. thiết bị của bạn. Ngoài ra, khi bạn duyệt trang web, chúng tôi thu thập thông tin về cá nhân
-                    trang web hoặc sản phẩm bạn xem, trang web hoặc cụm từ tìm kiếm nào đã giới thiệu bạn đến trang web và
-                    cách bạn
-                    đã tương tác với họ. trang web. Chúng tôi gọi thông tin được thu thập tự động này là “Thiết bị
-                    Thông tin". Hơn nữa, chúng tôi có thể thu thập dữ liệu cá nhân mà bạn cung cấp cho chúng tôi (bao gồm
-                    nhưng không giới hạn
-                    đến Tên, Họ, Địa chỉ, Thông tin Thanh toán, v.v.) để thực hiện thỏa thuận. </p>
-                <p> Tại sao chúng tôi xử lý dữ liệu của bạn? </p>
-                <p> Ưu tiên hàng đầu của chúng tôi là bảo mật dữ liệu khách hàng và do đó, chúng tôi chỉ có thể xử lý dữ
-                    liệu người dùng tối thiểu,
-                    chỉ trong phạm vi cần thiết để duy trì trang web. Thông tin tự động được thu thập chỉ là
-                    được sử dụng để xác định các trường hợp lạm dụng tiềm ẩn và thiết lập thông tin thống kê liên quan đến
-                    việc sử dụng
-                    trang mạng. Thông tin thống kê này không được tổng hợp theo cách khác để xác định bất kỳ người dùng cụ
-                    thể nào của
-                    hệ thống. </p>
-                <p> Bạn có thể truy cập trang web mà không cần cho chúng tôi biết bạn là ai hoặc tiết lộ bất kỳ thông tin
-                    nào
-                    nếu không, hãy xác định bạn là một cá nhân cụ thể, có thể nhận dạng được. Tuy nhiên, nếu bạn muốn sử
-                    dụng
-                    các tính năng của trang web hoặc bạn muốn nhận bản tin của chúng tôi hoặc cung cấp các chi tiết khác
-                    bằng cách điền vào
-                    ra một biểu mẫu, bạn có thể cung cấp cho chúng tôi dữ liệu cá nhân. , chẳng hạn như email, họ, tên,
-                    thành phố,
-                    nơi ở, tổ chức, số điện thoại. Bạn có thể chọn không cung cấp cho chúng tôi thông tin cá nhân của bạn
-                    dữ liệu, nhưng sau đó bạn có thể không tận dụng được các tính năng nhất định của trang web. Ví dụ,
-                    bạn sẽ không thể nhận bản tin của chúng tôi hoặc liên hệ trực tiếp với chúng tôi từ trang web. Người
-                    dùng là
-                    không chắc về thông tin cần thiết có thể liên hệ với chúng tôi qua <a style="color: blue"
+                <p> <b> The personal information that we collect:</b> </p>
+                <p> When you visit simplemanplus.com, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some cookies. to be installed on the device. your device. In addition, when you browse the website, we collect information about the individual website or product you view, which website or search term referred you to the website, and how you have interacted with them. webpage. We refer to this automatically collected information as “Device Information.” Furthermore, we may collect the personal data you provide to us (including but not limited to First Name, Last Name, Last Name, Last Name, Address, Payment Information, etc.) to execute the agreement. </p>
+                <p><b> Why do we process your data? </b></p>
+                <p> Our top priority is the security of customer data, and therefore, we can only process minimal user data, only to the extent necessary to maintain the website. The automatically collected information is used solely to identify potential abuse cases and to establish statistical information regarding the use of the website. This statistical information is not otherwise aggregated to identify any particular user of the system. </p>
+                <p> You may visit the website without telling us who you are or disclosing any information that would otherwise identify you as a specific, identifiable individual. However, if you wish to use the features of the website or you would like to receive our newsletter or provide other details by filling out a form, you may provide us with personal data. core. , such as email, last name, first name, city, place of residence, organization, phone number. You may choose not to provide us with your personal data, but then you may not be able to take advantage of certain features of the website. For example, you will not be able to receive our newsletter or contact us directly from the website. Users who are unsure about the required information can contact us via <a style="color: blue"
                         href="mailto: simplemanplusvn@gmail.com"> simplemanplusvn@gmail.com </a> </p>
-                <p> Quyền của bạn: </p>
-                <p> Bạn có các quyền sau đây liên quan đến dữ liệu cá nhân của mình: </p>
+
+                <p> <b>You have the following rights in relation to your personal data: </b> </p>
                 <ul>
-                    <li> Quyền được thông báo. </li>
-                    <li> Quyền truy cập. </li>
-                    <li> Quyền sửa chữa. </li>
-                    <li> Quyền xóa. </li>
-                    <li> Quyền giới hạn việc xử lý. </li>
-                    <li> Quyền được di chuyển dữ liệu. </li>
-                    <li> Quyền phản đối. </li>
-                    <li> Các quyền liên quan đến việc lập hồ sơ và ra quyết định tự động. </li>
+                    <li> Right to be notified.</li>
+                    <li> Access rights. </li>
+                    <li> Right to repair.</li>
+                    <li> Right to delete. </li>
+                    <li> Right to limit processing. </li>
+                    <li> Right to data portability. </li>
+                    <li> Right to object. </li>
+                    <li> Rights related to automated profiling and decision making. </li>
                 </ul>
-                <p> Nếu bạn muốn thực hiện quyền này, vui lòng liên hệ với chúng tôi bằng thông tin liên hệ bên dưới. </p>
-                <p> Ngoài ra, chúng tôi lưu ý rằng chúng tôi đang xử lý thông tin của bạn để đáp
-                    ứng
-                    các hợp đồng mà chúng tôi có thể có với bạn (ví dụ: nếu bạn đặt hàng qua trang web.) hoặc
-                    nếu không để theo đuổi lợi ích kinh doanh hợp pháp của chúng tôi được liệt kê ở trên. Ngoài ra, xin lưu
-                    ý rằng
-                    thông tin có thể được chuyển ra ngoài Châu Âu, bao gồm cả Canada và Hoa Kỳ. </p>
-                <p> Liên kết đến các trang web khác: </p>
-                <p> Trang web của chúng tôi có thể chứa các liên kết đến các trang web khác không do chúng tôi sở hữu hoặc
-                    kiểm soát. Xin lưu ý rằng
-                    chúng tôi không chịu trách nhiệm về các trang web khác như vậy hoặc các thông lệ về quyền riêng tư của
-                    bên thứ ba. Chúng tôi khuyến khích bạn
-                    lưu ý khi bạn rời khỏi trang web của chúng tôi và đọc các điều khoản về quyền riêng tư của mỗi trang web
-                    có thể thu thập
-                    thông tin cá nhân. </p>
-                <p> <b> Bảo mật thông tin: </b> </p>
-                <p> Chúng tôi giữ thông tin bạn cung cấp trên máy chủ của chúng tôi trong một môi trường được kiểm soát, an toàn, được bảo vệ khỏi
-                    truy cập trái phép, sử dụng hoặc tiết lộ. Chúng tôi giữ hành chính, kỹ thuật và vật lý hợp lý
-                    các biện pháp bảo vệ được áp dụng để bảo vệ chống lại việc truy cập trái phép, sử dụng, sửa đổi và tiết lộ thông tin cá nhân
-                    dữ liệu trong sự kiểm soát và lưu giữ của chúng tôi. I. Tuy nhiên, truyền dữ liệu qua Internet hoặc mạng không dây
-                    không thể được đảm bảo. </p>
-                <p> <b> Tiết lộ pháp lý: </b> </p>
-                <p> Chúng tôi sẽ tiết lộ bất kỳ thông tin nào chúng tôi thu thập, sử dụng hoặc nhận được nếu pháp luật yêu cầu hoặc cho phép, chẳng hạn như
-                    tuân thủ quy trình pháp lý tương tự của tòa án và khi chúng tôi tin rằng việc tiết lộ là cần thiết để bảo vệ
-                    lợi ích của chúng tôi, bảo vệ sự an toàn của bạn hoặc sự an toàn của người khác, điều tra gian lận hoặc phản hồi
-                    yêu cầu của chính phủ. </p>
-                <p> <b> Thông tin liên lạc: </b> </p>
-                <p> Nếu bạn muốn liên hệ với chúng tôi để hiểu thêm về Chính sách này hoặc muốn liên hệ với chúng tôi
-                    về bất kỳ vấn đề nào liên quan đến quyền cá nhân và Thông tin cá nhân của bạn, bạn có thể gửi email tới
+                <p> If you wish to exercise this right, please contact us using the contact information below. If you wish to exercise this right, please contact us using the contact information below. </p>
+                <p> In addition, we note that we are processing your information to fulfill contracts we may have with you (for example, if you place an order through the website.) or otherwise to pursue Our legitimate business interests are listed above. In addition, please note that information may be transferred outside of Europe, including Canada and the United States.</p>
+                <p> <b> Links to other websites:</b></p>
+                <p> Our website may contain links to other websites that are not owned or controlled by us. Please note that we are not responsible for such other websites or third-party privacy practices. We encourage you to take note when you leave our website and read the privacy statements of each website that may collect personal information. </p>
+                <p> <b> Information security: </b> </p>
+                <p> We keep the information you provide on our servers in a controlled, secure environment, protected from unauthorized access, use or disclosure. We keep reasonable administrative, technical and physical safeguards in place to protect against unauthorized access, use, modification and disclosure of personal information data in control. and our keep. I. However, data transmission over the Internet or wireless networks cannot be guaranteed. </p>
+                <p> <b>Legal Disclosure:</b> </p>
+                <p> We will disclose any information we collect, use or receive if required or permitted by law, such as compliance with a similar legal process by a court, and when we believe that the Disclosure is necessary to protect our interests, protect your safety or the safety of others, investigate fraud, or respond to a government request. </p>
+                <p> <b>Communications: </b> </p>
+                <p> If you would like to contact us to understand more about this Policy or would like to contact us regarding any matter regarding your privacy rights and Personal Information, you may send an email to
                     <a style="color: blue" href="mailto:simplemanplusvn@gmail.com"> simplemanplusvn@gmail.com </a> </p>
 
             </div>
